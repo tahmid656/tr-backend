a782bf2b774a1f70745b84feaedc8bcb2dfb0891
Tahmid Rahman <tahmidrahman.656@gmail.com>
2023-06-20 09:11:08 +0800

    feat(global): initialize project and add routes

---
+++ data/spotlight.js
@@ -0,0 +1,22 @@
+const spotlight = [
+    {
+        id: 1,
+        name: 'Florida Tulip Agro',
+        img: 'gal-1.png',
+        icons: ['Illustrator', 'Photoshop']
+    },
+    {
+        id: 2,
+        name: 'Florida Tulip Agro',
+        img: 'gal-1.png',
+        icons: ['Illustrator', 'Photoshop']
+    },
+    {
+        id: 3,
+        name: 'Florida Tulip Agro',
+        img: 'gal-1.png',
+        icons: ['Illustrator', 'Photoshop']
+    },
+]
+
+module.exports = spotlight;
